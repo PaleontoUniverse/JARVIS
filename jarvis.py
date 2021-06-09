56c96bf3834495b8ea6781012e4c9a956f12eacd
PaleontoUniverse <71916779+PaleontoUniverse@users.noreply.github.com>
2021-06-09 23:07:24 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -91,8 +91,34 @@ if __name__ == "__main__":
             speak("according to greek philospher DEMOCRITUS, no one can understand ones favourites so please type your favourite song on your own")
 
         elif 'where is' in query:
-            webbrowser.open("earth.google.com/web")
-            speak("sir don't be so lazy please type the location on your own")
+            from selenium import webdriver 
+from time import sleep 
+
+driver = webdriver.Chrome("/home/arun/Downloads/chromedriver") 
+driver.get("https://www.google.co.in/maps/@10.8091781,78.2885026,7z") 
+sleep(2) 
+
+def searchplace():
+	   Place=driver.find_element_by_class_name("tactile-searchbox-input")
+	   Place.send_keys(query)
+	   Submit=driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button")
+	   Submit.click()
+searchplace()
+
+def directions():
+	   sleep(10)	
+	   directions=driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[7]/div/div[1]/div/div/div[5]/div[1]/div/button")
+	   directions.click()
+directions()
+
+def find():
+         sleep(6)
+         find=driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/input")
+         find.send_keys("Tirunelveli")
+         sleep(2)
+         search=driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/button[1]")
+         search.click()
+find()
 
         elif 'hello' in query:
             speak("hello sir,,,, how can i help you")
